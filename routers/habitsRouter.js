7f8145aee40a5f68a5d5f5cd1d8334ebbf481f11
Allister Rooke <allisterg@gmail.com>
2018-11-26 21:06:02 -0500

    added mongodb, put endpoint working

--- routers/habitsRouter.js
+++ routers/habitsRouter.js
@@ -1,84 +1,90 @@
 const express = require('express');
 const router = express.Router();
+const {Day, Habit} = require('../models');
 
-let habits = [
-    { id: 0, habit: "Study Spanish", checked: false },
-    { id: 1, habit: "Read a novel", checked: false },
-    { id: 2, habit: "Programming", checked: false }
-]
+// let habits = [
+//     { id: 0, habit: "Study Spanish", checked: false },
+//     { id: 1, habit: "Read a novel", checked: false },
+//     { id: 2, habit: "Programming", checked: false }
+// ]
 
-let days = {
-    Monday: {
-        dayId: 1,
-        habits:
-            [
-                { id: 0, habit: "Study Spanish", checked: false },
-                { id: 1, habit: "Read a novel", checked: false },
-                { id: 2, habit: "Programming", checked: false }
-            ]
-    },
+// let days = {
+//     Monday: {
+//         dayId: 1,
+//         habits:
+//             [
+//                 { id: 0, habit: "Study Spanish", checked: false },
+//                 { id: 1, habit: "Read a novel", checked: false },
+//                 { id: 2, habit: "Programming", checked: false }
+//             ]
+//     },
 
-    Tuesday: {
-        dayId: 2,
-        habits:
-            [
-                { id: 0, habit: "Study Spanish", checked: false },
-                { id: 1, habit: "Read a novel", checked: false },
-                { id: 2, habit: "Programming", checked: false }
-            ]
-    },
-    Wednesday: {
-        dayId: 3,
-        habits:
-            [
-                { id: 0, habit: "Study Spanish", checked: false },
-                { id: 1, habit: "Read a novel", checked: false },
-                { id: 2, habit: "Programming", checked: false }
-            ]
-    },
-    Thursday: {
-        dayId: 4, 
-        habits:
-            [
-                { id: 0, habit: "Study Spanish", checked: false },
-                { id: 1, habit: "Read a novel", checked: false },
-                { id: 2, habit: "Programming", checked: false }
-            ]
-    },
-    Friday: {
-        dayId: 5,
-        habits:
-            [
-                { id: 0, habit: "Study Spanish", checked: false },
-                { id: 1, habit: "Read a novel", checked: false },
-                { id: 2, habit: "Programming", checked: false }
-            ]
-    },
-    Saturday: {
-        dayId: 6,
-        habits:
-            [
-                { id: 0, habit: "Study Spanish", checked: false },
-                { id: 1, habit: "Read a novel", checked: false },
-                { id: 2, habit: "Programming", checked: false }
-            ]
-    },
-    Sunday: {
-        dayId: 7,
-        habits:
-            [
-                { id: 0, habit: "Study Spanish", checked: false },
-                { id: 1, habit: "Read a novel", checked: false },
-                { id: 2, habit: "Programming", checked: false }
-            ]
-    }
-}
+//     Tuesday: {
+//         dayId: 2,
+//         habits:
+//             [
+//                 { id: 0, habit: "Study Spanish", checked: false },
+//                 { id: 1, habit: "Read a novel", checked: false },
+//                 { id: 2, habit: "Programming", checked: false }
+//             ]
+//     },
+//     Wednesday: {
+//         dayId: 3,
+//         habits:
+//             [
+//                 { id: 0, habit: "Study Spanish", checked: false },
+//                 { id: 1, habit: "Read a novel", checked: false },
+//                 { id: 2, habit: "Programming", checked: false }
+//             ]
+//     },
+//     Thursday: {
+//         dayId: 4, 
+//         habits:
+//             [
+//                 { id: 0, habit: "Study Spanish", checked: false },
+//                 { id: 1, habit: "Read a novel", checked: false },
+//                 { id: 2, habit: "Programming", checked: false }
+//             ]
+//     },
+//     Friday: {
+//         dayId: 5,
+//         habits:
+//             [
+//                 { id: 0, habit: "Study Spanish", checked: false },
+//                 { id: 1, habit: "Read a novel", checked: false },
+//                 { id: 2, habit: "Programming", checked: false }
+//             ]
+//     },
+//     Saturday: {
+//         dayId: 6,
+//         habits:
+//             [
+//                 { id: 0, habit: "Study Spanish", checked: false },
+//                 { id: 1, habit: "Read a novel", checked: false },
+//                 { id: 2, habit: "Programming", checked: false }
+//             ]
+//     },
+//     Sunday: {
+//         dayId: 7,
+//         habits:
+//             [
+//                 { id: 0, habit: "Study Spanish", checked: false },
+//                 { id: 1, habit: "Read a novel", checked: false },
+//                 { id: 2, habit: "Programming", checked: false }
+//             ]
+//     }
+// }
 
 
 
 router.get('/api/habits', (req, res) => {
-    console.log(days)
-    res.json(days)
+    Day.find({})
+    .populate('habits.habit')
+    .then(data => {
+        console.log(data)
+        res.json(data)
+    })
+    
 })
 
 router.post('/api/habits', (req, res) => {
@@ -89,13 +95,31 @@ router.post('/api/habits', (req, res) => {
 
 })
 
-router.put('/api/habits', (req, res) => {
-    const id = req.body.id
-    const day = req.body.day
-    foundHabit = days[day].habits.find(habit => habit.id === id)
-    foundHabit.checked = !foundHabit.checked
-    res.json(foundHabit)
-})
+router.put('/api/habits', (req, res, next) => {
+    const habitId = req.body.id
+    console.log(habitId)
+    const dayId = req.body.day
+    console.log(dayId)
+    Day.findById(dayId)
+    .populate('habits.habit')
+    .then(_day => {
+        let day = _day;    
+        console.log('line 106', day)
+            const foundHabit = day.habits.find(val => val.habit._id.toString() === habitId)
+            console.log('foundHabit', foundHabit);
+            foundHabit.checked = !foundHabit.checked;
+            day.save();
+            
+            console.log('line 113', day)
+            res.json(day)
+        })
+    });
+    
+  
+
+    
+
+    
 
 
 module.exports = router;
