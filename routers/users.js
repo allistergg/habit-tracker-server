9a622dab3eb8a4db008d621c8f6889a73aca50e6
Allister Rooke <allisterg@gmail.com>
2018-12-07 16:49:20 -0500

    updated users endpoint length error message

--- routers/users.js
+++ routers/users.js
@@ -73,7 +73,7 @@ router.post('/', (req, res, next) => {
       code: 422,
       reason: 'ValidationError',
       message: tooSmallField
-        ? `Must be at least ${sizedFields[tooSmallField]
+        ? `${tooSmallField} Must be at least ${sizedFields[tooSmallField]
           .min} characters long`
         : `Must be at most ${sizedFields[tooLargeField]
           .max} characters long`,
